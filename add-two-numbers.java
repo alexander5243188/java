12c8fcd49cf387df0c2e10c8a48c03f25ab9620d
Alexander Richard Vega Fuentes <alexander@localhost.localdomain>
2024-03-07 16:00:03 -0400

    Suma de dos valores

---
+++ add-two-numbers.java
@@ -0,0 +1,7 @@
+class addTwoNumbers {
+    public static void main(String[] args){
+        int num1 = 2, num2 = 10, sum;
+        sum = num1 + num2;
+        System.out.println("Sum of these numbers: "+sum);
+    }
+}
