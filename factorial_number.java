dd2d81efd76c9b627ea3ad41fda2700a96ddce42
Alexander Richard Vega Fuentes <alexander5243188@yahoo.es>
2024-03-10 14:08:11 -0400

    Se añadieron las varialbles y el ciclo for

--- factorial_number.java
+++ factorial_number.java
@@ -1,3 +1,14 @@
 public class factorial_number {
-    
+    public static void main (String[] args){
+        // valor para la operacion del factorial
+        int numero = 5;
+
+        // inicializar el factorial en 1
+        long factorial = 1;
+
+        for (int i = 1; i < numero; i++) {
+            factorial *= i;
+        }
+        System.out.println("Factorial de "+numero+ " = "+factorial);
+    }
 }
